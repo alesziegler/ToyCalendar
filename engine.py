efdfab26aa1e52b8b9e6bad2193e07e68a06789e
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-09-13 15:39:11 +0000

    Intermediate save

---
+++ engine.py
@@ -0,0 +1,9 @@
+from datetime import date, timedelta
+
+start_date = date(2024, 1, 1)
+end_date = date(2024, 12, 31)
+delta = end_date - start_date
+
+for i in range(delta.days):
+  # in function, this maybe needs a list comprehension?
+  print( (start_date + timedelta(days=i+1)).strftime("%d %B") )

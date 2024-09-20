0be60204ad2a067f5d112f3e53a859537f81392d
trawn <trawn@seznam.cz>
2024-09-20 12:40:21 +0000

    with new IDE

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ print(type(start_date))
 test_file_manip = FileManipulation("svatky.txt")
 
 test_file_manip.delete_empty_lines()
+print("test")
 
 """
 Ok, we need a method that copies file and deletes empty lines from it.
